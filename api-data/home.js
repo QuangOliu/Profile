a6dc315b1eddc47b49846c3c0c93b494206e8841
Quang Oliu <trantran30102002@gmail.com>
2022-06-03 10:26:33 +0700

    update typing

--- api-data/home.js
+++ api-data/home.js
@@ -1,21 +1,27 @@
 export const dataHome = {
   img: '/assets/images/anhavt.jpg',
-  hello: "Hello, I'm Quang Oliu",
-  typing: ["Hi, I'm Quang", "I'm 20 years old","I'm handsome NO.1", 'Right ?????'],
-  about: "I'm a intern Web developer In Sentius, over 3 months of professional experience.",
+  hello: `Hello, I'm Quang Oliu`,
+  typing: [
+    "Hi, I'm  <span style=color:red>Quang</span>",
+    "I'm <span style=color:red>20</span> years old",
+    "I'm <span style=color:red>handsome</span> NO.1",
+    '<span style=color:red>Right ?????</span>',
+  ],
+  about:
+    "I'm a intern Web developer In Sentius, over 3 months of professional experience.",
   social: [
     {
-      id:1,
-      type:'facebook',
+      id: 1,
+      type: 'facebook',
       src: '/assets/images/facebook.png',
-      url:'https://www.facebook.com/QuangOliu',
+      url: 'https://www.facebook.com/QuangOliu',
     },
     {
-      id:2,
+      id: 2,
       type: 'instagram',
       src: '/assets/images/instagram.png',
-      url:'https://www.instagram.com/quang.oliu/',
+      url: 'https://www.instagram.com/quang.oliu/',
     },
   ],
-  CV:'#'
+  CV: '#',
 };
